45fec36c59af96e348e7a802823c9bd4f3e4b224
mie-h <56993977+mie-h@users.noreply.github.com>
2022-12-16 17:50:41 -0800

    get data from kaggle and preprocess

---
+++ gpt3-sentiment-analysis/text_sentiment.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import jsonlines
+import openai
+import numpy as np
+import re
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+
+openai.api_key='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'
+
+
+DATASET_COLUMNS  = ["sentiment", "ids", "date", "flag", "user", "text"]
+DATASET_ENCODING = "ISO-8859-1"
+dataset = pd.read_csv('training.1600000.processed.noemoticon.csv',
+                      encoding=DATASET_ENCODING , names=DATASET_COLUMNS)
+
+
+dataset = dataset[:1000]
+print(dataset)
+
+# Removing the unnecessary columns.
+dataset = dataset[['sentiment','text']]
+# Replacing the values to ease understanding.
+dataset['sentiment'] = dataset['sentiment'].replace(4,1)
+
+def convert_labels(label):
+  if label == 0:
+    return 'Negative'
+  if label == 2:
+    return 'Neutral'
+  if label == 4:
+    return 'Positive'
+
+dataset['sentiment'] = dataset['sentiment'].apply(convert_labels)
+
+print(dataset.head())
+
+emojis = {':)': 'smile', ':-)': 'smile', ';d': 'wink', ':-E': 'vampire', ':(': 'sad', 
+          ':-(': 'sad', ':-<': 'sad', ':P': 'raspberry', ':O': 'surprised',
+          ':-@': 'shocked', ':@': 'shocked',':-$': 'confused', ':\\': 'annoyed', 
+          ':#': 'mute', ':X': 'mute', ':^)': 'smile', ':-&': 'confused', '$_$': 'greedy',
+          '@@': 'eyeroll', ':-!': 'confused', ':-D': 'smile', ':-0': 'yell', 'O.o': 'confused',
+          '<(-_-)>': 'robot', 'd[-_-]b': 'dj', ":'-)": 'sadsmile', ';)': 'wink', 
+          ';-)': 'wink', 'O:-)': 'angel','O*-)': 'angel','(:-D': 'gossip', '=^.^=': 'cat'}
+
+def preprocess(text):
+    processedText = []
+    
+    # Create Lemmatizer and Stemmer.
+    wordLemm = WordNetLemmatizer()
+    
+    # Defining regex patterns.
+    urlPattern        = r"((http://)[^ ]*|(https://)[^ ]*|( www\.)[^ ]*)"
+    userPattern       = '@[^\s]+'
+    alphaPattern      = "[^a-zA-Z0-9]"
+    sequencePattern   = r"(.)\1\1+"
+    seqReplacePattern = r"\1\1"
+    
+    for tweet in text:
+        tweet = tweet.lower()
+        
+        # Replace all URls with 'URL'
+        tweet = re.sub(urlPattern,' URL',tweet)
+        # Replace all emojis.
+        for emoji in emojis.keys():
+            tweet = tweet.replace(emoji, "EMOJI" + emojis[emoji])        
+        # Replace @USERNAME to 'USER'.
+        tweet = re.sub(userPattern,' USER', tweet)        
+        # Replace all non alphabets.
+        tweet = re.sub(alphaPattern, " ", tweet)
+        # Replace 3 or more consecutive letters by 2 letter.
+        tweet = re.sub(sequencePattern, seqReplacePattern, tweet)
+
+        tweetwords = ''
+        for word in tweet.split():
+            # Checking if the word is a stopword.
+#             if word not in stopwordlist:
+                if len(word)>1:
+                    # Lemmatizing the word.
+                    word = wordLemm.lemmatize(word)
+                    tweetwords += (word+' ')
+            
+        processedText.append(tweetwords)
+        
+    return processedText
+
+text = list(dataset['text'])
+sentiment = list(dataset['sentiment'])
+processedtext = preprocess(text)
+print(type(text[0]))
+print(type(sentiment[0]))
+
+
+with jsonlines.open('train.jsonl', mode='w') as writer:
+  for i in range(len(sentiment)):
+    writer.write({
+      'text': processedtext[i],
+      'label': sentiment[i]
+    })
